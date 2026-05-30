b8f1fe9fe76edad6e8d3d8eef96d95f29d92d232
Md. Mehedi Hasan <mehedi85879@gmail.com>
2026-05-30 22:23:43 +0600

    Day 7 Done

---
+++ Day 7/A_Ultra_Fast_Mathematician.cpp
@@ -0,0 +1,36 @@
+// - Md. Mehedi Hasan
+
+#include <bits/stdc++.h>
+#define Md_Mehedi_Hasan ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define vll vector<long long>
+#define ip(x) for (auto &it : x) cin >> it
+#define all(x) x.begin(), x.end()
+using ll = long long;
+using ull = unsigned long long;
+
+void solve()
+{
+    string s1, s2, xorReseult = ""; cin >> s1 >> s2;
+    for(int i = 0; i < s1.length(); i++)
+        xorReseult += (s1[i] == s2[i] ? '0' : '1');
+    cout << xorReseult;
+}
+
+int main()
+{
+    Md_Mehedi_Hasan;
+
+    solve();
+
+    return 0;
+}
+
+// https://codeforces.com/problemset/problem/61/A
