05919225bb021f97fbec700848a52300757d053b
Md. Mehedi Hasan <mehedi85879@gmail.com>
2026-05-30 17:34:34 +0600

    Day 6 Done

---
+++ Day 6/A_George_and_Accommodation.cpp
@@ -0,0 +1,39 @@
+// - Md. Mehedi Hasan
+
+#include <bits/stdc++.h>
+#define Md_Mehedi_Hasan ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define vll vector<long long>
+#define ip(x) for (auto &it : x) cin >> it
+#define all(x) x.begin(), x.end()
+using ll = long long;
+using ull = unsigned long long;
+
+void solve()
+{
+    int n, cnt = 0; cin >> n;
+    while(n--)
+    {
+        int p, q; cin >> p >> q;
+        if(q - p >= 2) cnt++;
+    }
+    cout << cnt;
+}
+
+int main()
+{
+    Md_Mehedi_Hasan;
+
+    solve();
+
+    return 0;
+}
+
+// https://codeforces.com/problemset/problem/467/A
